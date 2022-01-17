307aabd7bd973dac3ac0e27c7f95e3aceb9fa72f
iradukunda1 <iradujdd4@gmail.com>
2022-01-17 14:18:26 +0200

    create service initialization

--- email/email.go
+++ email/email.go
@@ -3,20 +3,21 @@ package email
 import (
 	"fmt"
 	"net/smtp"
+	"strconv"
 )
 
 type (
 	// Email contains all the details about the email to be sent
 	Email struct {
-		Subject    string // Unique id for the current Email
-		Body       string // The content of the Email
-		Sender     string
+		Subject    string   // Unique id for the current Email
+		Body       string   // The content of the Email
 		Recipients []string // The recipients of this particular Email
 	}
+
 	Config struct {
 		Sender string
 		Host   string
-		Port   string
+		Port   int64
 		Secret string
 	}
 	// SendService is the principal interface.
@@ -24,11 +25,31 @@ type (
 	SendService interface {
 		Send(s *Config, email *Email) error
 	}
+
+	//Client struct hold auth client
+	Client struct {
+		Auth   smtp.Auth
+		Host   string
+		Sender string
+		Port   int64
+	}
 )
 
-func Send(s *Config, email *Email) error {
+// Initailize email-client by provinding sender config
+func New(c *Config) *Client {
+	return &Client{
+		Auth:   smtp.PlainAuth("", c.Sender, c.Secret, c.Host),
+		Host:   c.Host,
+		Port:   c.Port,
+		Sender: c.Sender,
+	}
 
-	auth := smtp.PlainAuth("", s.Sender, s.Secret, s.Host)
+}
+
+// Send that handler send email with it's subject
+func (s *Client) Send(email *Email) error {
+
+	// auth := smtp.PlainAuth("", s.Sender, s.Secret, s.Host)
 
 	to := fmt.Sprintf("To:%s\r\n", email.Recipients[0:])
 
@@ -37,7 +58,7 @@ func Send(s *Config, email *Email) error {
 	message := []byte(to + subject + "\r\n" + email.Body)
 
 	// Send actual email
-	err := smtp.SendMail(s.Host+":"+s.Port, auth, email.Sender, email.Recipients, message)
+	err := smtp.SendMail(s.Host+":"+strconv.Itoa(int(s.Port)), s.Auth, s.Sender, email.Recipients, message)
 
 	if err != nil {
 		return err
